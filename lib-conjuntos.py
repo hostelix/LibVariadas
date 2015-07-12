6eeb50f2c3234d57c20c4817c325fd08f50cecb5
Israel Lugo <hostelixisrael@gmail.com>
2015-07-12 15:25:54 -0430

    add lib-conjuntos.py

---
+++ lib-conjuntos.py
@@ -0,0 +1,23 @@
+######## LEYENDA ########
+#   $Ca = Conjunto A
+#	$Cb = Conjunto B
+#	$S = Espacio muestral
+
+
+def union(Ca, Cb):
+	return  list(Ca)+(filter(lambda x: x not in Ca, list(Cb)))
+	
+def intercepcion(Ca,Cb):
+	return filter(lambda x: x in a, list(b))
+
+def espacio(*conjuntos):
+	result = []
+	for n in conjuntos:
+		result = list(union(result,n))
+	return result
+
+def complemento(Ca, S):
+	return filter(lambda x: x not in Ca, list(S))
+
+def diferencia(Ca,Cb):
+	return filter(lambda x: x not in Cb, list(Ca))
